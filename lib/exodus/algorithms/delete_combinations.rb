1ca733a06f0dec0fb1ca5fad4946d256be5aed80
x13enos <x3enos@gmail.com>
2015-04-11 22:13:53 +0300

    [^] change logic for delete and update gems algorithm

--- lib/exodus/algorithms/delete_combinations.rb
+++ lib/exodus/algorithms/delete_combinations.rb
@@ -1,8 +1,9 @@
 class Exodus::Algorithms::DeleteCombinations
-  attr_accessor :gems_position, :board
+  attr_accessor :gems_position, :board, :new_positions
   def initialize(board, gems_position)
     @board = board
     @gems_position = gems_position
+    @new_positions = {}
   end
 
   def delete_able?
@@ -12,9 +13,11 @@ class Exodus::Algorithms::DeleteCombinations
   def perform
     result = []
     while(delete_able?) do
+      new_positions = {}
       delete_gems(indexes_removes_gems)
       result << { :delete_gems => indexes_removes_gems, :new_gems_position => add_new_gems }
     end
+    Rails.logger.info result
     return result
   end
 
@@ -29,18 +32,19 @@ class Exodus::Algorithms::DeleteCombinations
   end
 
   def add_new_gems
-    h = {}
+    h = { :move_gems => [], :new_gems => [] }
     (0..63).to_a.reverse.each do |i|
       if gems_position[i.to_s].nil?
         c_i = i
         while (gems_position[i.to_s].nil? && c_i / 8 >= 0) do
           c_i -= 8
           if gems_position[c_i.to_s].present?
-            h[i.to_s] = update_gem_position(i.to_s, gems_position[c_i.to_s])
-            gems_position[c_i.to_s] = nil
+            h[:move_gems] << move_gem(i.to_s, c_i.to_s)
           end
         end
-        h[i.to_s] = update_gem_position(i.to_s, (0..6).to_a.sample) if gems_position[i.to_s].nil?
+        if gems_position[i.to_s].nil?
+          h[:new_gems] << create_new_gem(i.to_s)
+        end
       end
     end
     return h
@@ -49,6 +53,33 @@ class Exodus::Algorithms::DeleteCombinations
   def update_gem_position(index, new_gem)
     gems_position[index] = new_gem
     return new_gem
+  end
+
+  def move_gem(current_gem_index, move_gem_index)
+    t = { :type => gems_position[move_gem_index.to_s],
+          :index => current_gem_index,
+          :old_index => move_gem_index  }
+    gems_position[move_gem_index.to_s] = nil
+    update_gem_position(t[:index], t[:type])
+    t
+  end
+
+  def create_new_gem(index)
+    random_gem = (0..6).to_a.sample
+    update_new_position_variables(index)
+    update_gem_position(index, random_gem)
+    { :type => random_gem,
+      :index => index,
+      :position => new_positions[index.to_i % 8] }
+  end
+
+  def update_new_position_variables(index)
+    column = index.to_i % 8
+    if new_positions[column]
+      new_positions[column] += 1
+    else
+      new_positions[column] = 1
+    end
   end
 
   def combination_by_horizontal
