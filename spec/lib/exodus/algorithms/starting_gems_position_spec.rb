438c6eba6c73f735ffb751f21db556d8e614bbd5
x13enos <x3enos@gmail.com>
2015-04-19 10:25:59 +0300

    [^] added spec to starting_gems algorithm

--- spec/lib/exodus/algorithms/starting_gems_position_spec.rb
+++ spec/lib/exodus/algorithms/starting_gems_position_spec.rb
@@ -18,6 +18,28 @@ describe Exodus::Algorithms::StartingGemsPosition do
   describe "#perform" do
     let(:game) { create(:game) }
     let(:lib) { Exodus::Algorithms::StartingGemsPosition.new(game.id) }
+
+    it "should return false if gems not have ability to deleting" do
+      lib.perform
+      board = Board.last
+      delete_gems_lib = Exodus::Algorithms::DeleteCombinations.new(board, board.gems_position)
+      expect(delete_gems_lib.delete_able?).to be_falsey
+    end
+
+    it "should create board" do
+      Board.delete_all
+      lib.perform
+      expect(Board.count).to eq(1)
+    end
+
+    it "should return gems" do
+      expect(lib.perform).to eq(lib.gems)
+    end
+
+    it "should return 64 length" do
+      lib.perform
+      expect(lib.gems.count).to eq(64)
+    end
   end
 
 end
